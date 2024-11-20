0e8e90e63002b5c4a78a3ef42f31a3782a93a0a2
Jorge Marcos <jorge.marcosfer@gmail.com>
2024-11-20 21:40:33 +0100

    remove head on individual demos and add rescoped css to parent landing page html

--- examples/demos/mood-classifiers/vite.config.js
+++ examples/demos/mood-classifiers/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+    base: "/demos/mood-classifiers/dist/",
     server: {
         https: true,
         headers: {
